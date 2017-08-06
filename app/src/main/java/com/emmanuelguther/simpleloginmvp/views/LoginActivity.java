4ce4e55c504424698424842a2aff9fe790a12df2
Emmanuel <Emmanuelguther@gmail.com>
2017-08-06 19:09:17 +0200

    UI

--- app/src/main/java/com/emmanuelguther/simpleloginmvp/views/LoginActivity.java
+++ app/src/main/java/com/emmanuelguther/simpleloginmvp/views/LoginActivity.java
@@ -1,17 +1,34 @@
 package com.emmanuelguther.simpleloginmvp.views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.emmanuelguther.simpleloginmvp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.editText)
+    EditText editText;
+    @BindView(R.id.editText2)
+    EditText editText2;
+    @BindView(R.id.imageButton)
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
 
+    @OnClick(R.id.imageButton)
+    public void onViewClicked() {
+    }
 }
